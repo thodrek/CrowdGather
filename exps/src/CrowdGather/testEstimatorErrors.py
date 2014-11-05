b395930c5d6da898bbc010bd5218438d6b344aee
Thodoris Rekatsinas <thodrek@cs.umd.edu>
2014-11-05 14:25:01 -0500

    updates

--- exps/src/CrowdGather/testEstimatorErrors.py
+++ exps/src/CrowdGather/testEstimatorErrors.py
@@ -67,6 +67,7 @@ for conf in configurations:
             actualReturn = newUnique - oldUnique
 
             # errors
+            print abs(estChao - actualReturn), abs(estRegr - actualReturn), abs(estNewRegr - actualReturn)
             totalErrorChao += abs(estChao - actualReturn)/float(actualReturn+1.0)
             totalErrorRegr += abs(estRegr - actualReturn)/float(actualReturn+1.0)
             totalErrorNew += abs(estNewRegr - actualReturn)/float(actualReturn+1.0)
