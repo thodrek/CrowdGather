4c11266bcf200a903c5330bdf4a8fc4c079a4b93
Thodoris Rekatsinas <thodrek@cs.umd.edu>
2014-11-05 14:49:32 -0500

    updates

--- exps/src/CrowdGather/PointEstimateNew.py
+++ exps/src/CrowdGather/PointEstimateNew.py
@@ -164,7 +164,9 @@ class PointEstimateNew:
 
         # check if sample is empty
         if self.sampleSize == 0.0:
+            print "New, empty sample"
             if self.point.emptyPopulation == True:
+                print "New, empty population"
                 return 0.0
             else:
                 return self.querySize
