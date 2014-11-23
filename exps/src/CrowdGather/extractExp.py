45e99e80f40e79da4e2ece8a5d7cf5b7722f618a
Thodoris Rekatsinas <thodrek@cs.umd.edu>
2014-11-23 11:18:32 -0500

    updates

--- exps/src/CrowdGather/extractExp.py
+++ exps/src/CrowdGather/extractExp.py
@@ -30,7 +30,8 @@ def main(argv):
     budget = int(argv[1])
 
     # set query configurations
-    configurations = [(5,0),(10,0),(20,0),(50,0),(100,0),(5,2),(10,2),(10,5),(20,2),(20,5),(20,10),(50,2),(50,5),(50,10),(50,20),(100,2),(100,5),(100,10),(100,20),(100,50)]
+    #configurations = [(5,0),(10,0),(20,0),(50,0),(100,0),(5,2),(10,2),(10,5),(20,2),(20,5),(20,10),(50,2),(50,5),(50,10),(50,20),(100,2),(100,5),(100,10),(100,20),(100,50)]
+    configurations = [(5,0),(10,0),(20,0),(5,2),(10,5),(20,5),(20,10)]
     #configurations = [(10,5)]
     # initialize new EntityExtraction
 
@@ -38,7 +39,7 @@ def main(argv):
 
     for eMethod in extractionMethods:
         for est in estimator:
-            eExtract = EntityExtraction.EntityExtraction(budget,hList,hDescr,itemInfo,configurations,100,50,eMethod,est,newLattice)
+            eExtract = EntityExtraction.EntityExtraction(budget,hList,hDescr,itemInfo,configurations,20,10,eMethod,est,newLattice)
 
             gain, cost = eExtract.retrieveItems()
             print "EstMethod, est, Gain, cost",eMethod,est,gain,cost
