d0ec7bff385854ce817f79948b1bf70fa564f3ea
Thodoris Rekatsinas <thodrek@cs.umd.edu>
2014-11-23 22:12:02 -0500

    updates

--- exps/src/CrowdGather/EntityExtraction.py
+++ exps/src/CrowdGather/EntityExtraction.py
@@ -209,9 +209,9 @@ class EntityExtraction:
             if bestAction:
                 actualGain = bestAction.takeAction()
                 gain += actualGain
-                print "Took:", bestAction.point.getKey(),"with qS:",bestAction.querySize,"and exS:",bestAction.excludeListSize
-                print "Actual gain was:", actualGain
-                print "Predicted gain was:", bestGain
+                #print "Took:", bestAction.point.getKey(),"with qS:",bestAction.querySize,"and exS:",bestAction.excludeListSize
+                #print "Actual gain was:", actualGain
+                #print "Predicted gain was:", bestGain
                 cost += bestAction.computeCost(self.maxQuerySize,self.maxExListSize)
             else:
                 print "No good action found."
