a1254ea2cd8b8f00fb800c6b672c790e79941c7c
Thodoris Rekatsinas <thodrek@cs.umd.edu>
2014-11-26 11:24:02 -0500

    test

--- exps/src/CrowdGather/SomeClass.py
+++ exps/src/CrowdGather/SomeClass.py
@@ -1,29 +1,29 @@
 from multiprocessing import Pool
-import SomeOtherClass
+import PointEstimateNew
 import math
 class someClass(object):
 
-	def __init__(self):
-		pass
+    def __init__(self):
+        pass
 
-	def f(self, args):
-		#can put something expensive here to verify CPU utilization
-		e, round, mQ, mL = args
-		cost = e.computeCost(mQ,mL)
-		gain, variance, upperGain, lowerGain = e.estimateGain(True)
-	        armGain = gain + math.sqrt(variance*math.log(round)/e.timesSelected)
-	        gainCostRatio = float(armGain)/float(cost)
-        	return gainCostRatio
+    def f(self, args):
+        #can put something expensive here to verify CPU utilization
+        e, round, mQ, mL = args
+        cost = e.computeCost(mQ,mL)
+        gain, variance, upperGain, lowerGain = e.estimateGain(True)
+        armGain = gain + math.sqrt(variance*math.log(round)/e.timesSelected)
+        gainCostRatio = float(armGain)/float(cost)
+        return gainCostRatio
  
-	def go(self,s):
-		pool = Pool(processes=3)
-		results = pool.map(self.f,s)
-		return results
+    def go(self,s):
+        pool = Pool(processes=3)
+        results = pool.map(self.f,s)
+        return results
 
-	def run(self):
-		s = []
-		for i in range(10):
-			for j in range(10):
-				newS = SomeOtherClass.SomeOtherClass(i,j,"shenRegression")
-				s.append((newS,1.0,10,10))
-		return self.go(s)
+    def run(self):
+        s = []
+        for i in range(10):
+            for j in range(10):
+                newS = PointEstimateNew.PointEstimateNew(None,i,j,"shenRegression")
+                s.append((newS,1.0,10,10))
+        return self.go(s)
