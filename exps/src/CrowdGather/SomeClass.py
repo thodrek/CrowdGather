34d33816ccd09e4ed999c5edd59948513f68eaa0
Thodoris Rekatsinas <thodrek@cs.umd.edu>
2014-11-26 14:08:21 -0500

    test

--- exps/src/CrowdGather/SomeClass.py
+++ exps/src/CrowdGather/SomeClass.py
@@ -26,6 +26,6 @@ class someClass(object):
         s = []
         for i in range(1,10):
             for j in range(1,10):
-                newS = PointEstimateShenTest.PointEstimateShenTest("root",i,j,"shenRegression")
+                newS = PointEstimateShenTest.PointEstimateShenTest("||",i,j,"shenRegression")
                 s.append((newS,1.0,10,10))
         return self.go(s)
