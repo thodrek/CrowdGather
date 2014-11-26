9f5d250dbc6bb0b550cbf50d6936cef09db8c8cb
Thodoris Rekatsinas <thodrek@cs.umd.edu>
2014-11-26 11:07:39 -0500

    test

--- exps/src/CrowdGather/test1.py
+++ exps/src/CrowdGather/test1.py
@@ -23,12 +23,6 @@ def _pickle_method(method):
     return _unpickle_method, (func_name, obj, cls)
 
 
-def _pickle_method(method):
-     func_name = method.im_func.__name__
-     obj = method.im_self
-     cls = method.im_class
-     return _unpickle_method, (func_name, obj, cls)
-
 def _unpickle_method(func_name, obj, cls):
     for cls in cls.__mro__:
         try:
