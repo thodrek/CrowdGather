68e0a8c7f70c008a258df108f1eeff9ae30e0358
Thodoris Rekatsinas <thodrek@cs.umd.edu>
2014-11-28 12:02:30 -0500

    test

--- exps/src/CrowdGather/PointEstimateNew.py
+++ exps/src/CrowdGather/PointEstimateNew.py
@@ -432,6 +432,7 @@ class PointEstimateNew:
                     #returnEstimates.append(self.querySize)
 
             elif not self.oldK:
+                print self.point.sampleLogs
                 Chat = self.estimateCoverage()
                 gain = f0*(1.0 - (1.0 - (1.0 - Chat)/(f0 + 1.0))**self.querySize)
                 returnEstimates.append(gain)
