d74583e08d8e63d87b710e24d9d439d52a12f353
Thodoris Rekatsinas <thodrek@cs.umd.edu>
2015-02-09 13:49:27 +0100

    Berk

--- exps/src/CrowdGather/extractExp.py
+++ exps/src/CrowdGather/extractExp.py
@@ -11,7 +11,7 @@ if __name__ == "__main__":
     estimator = ["chao92"]
     #extractionMethods = ["BFS","GS_thres"]
     #extractionMethods = ["BFS", "BerkBaseline", "GS_thres", "GS_thres_NoEx"]
-    extractionMethods = ["BerkBaseline", "GS_thres"]
+    extractionMethods = ["BerkBaseline"]
     #estimator = ["chao92"]
     # construct hierarchy list
     catH = pickle.load(open("/scratch0/Dropbox/Eventbrite/eventsHierarchies/categoryHierarchy.pkl","rb"))
@@ -29,8 +29,8 @@ if __name__ == "__main__":
     newLattice = Lattice.Lattice(hList,hDescr,itemInfo)
 
     # set budget
-    #budgetValues = [10,20,50,80,100]
-    budgetValues = [20,80]
+    budgetValues = [10,20,50,80,100]
+    #budgetValues = [20,80]
 
     # set query configurations
     #configurations = [(5,0),(10,0),(20,0),(50,0),(100,0),(5,2),(10,2),(10,5),(20,2),(20,5),(20,10),(50,2),(50,5),(50,10),(50,20),(100,2),(100,5),(100,10),(100,20),(100,50)]
@@ -47,7 +47,7 @@ if __name__ == "__main__":
             if eMethod in ["random", "randomLeaves", "BFS", "BerkBaseline"]:
                 gainValues = []
                 costValues = []
-                for i in range(10):
+                for i in range(2):
                     eExtract = EntityExtraction.EntityExtraction(b,hList,hDescr,itemInfo,configurations,20,10,eMethod,"chao92",newLattice)
                     gain, cost, actionSelected, gainHist, costHist = eExtract.retrieveItems()
                     gainValues.append(gain)
